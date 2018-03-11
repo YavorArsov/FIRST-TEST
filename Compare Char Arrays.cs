d4f9a89b4d99e7b8ec3a9f3c06a5bdb2c75a8a07
YavorArsov <iavor_venus@abv.bg>
2018-03-11 10:34:42 +0200

    Kaчвам едно от домашните

---
+++ Compare Char Arrays.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Compare_Char_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] wordOne = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+            char[] wordTwo = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse)
+                .ToArray();
+            int MaxChar = Math.Min(wordOne.Length, wordTwo.Length);
+            for (int i = 0; i < MaxChar; i++)
+            {
+                if (wordOne[i] < wordTwo[i])
+                {
+                    Console.WriteLine(string.Join("", wordOne));
+                    Console.WriteLine(string.Join("", wordTwo));
+                    break;
+                }
+                else if (wordOne[i] > wordTwo[i])
+                {
+                    Console.WriteLine(string.Join("", wordTwo));
+                    Console.WriteLine(string.Join("", wordOne));
+                    break;
+                }
+                else if (wordOne.Length < wordTwo.Length)
+                {
+                    Console.WriteLine(string.Join("", wordOne));
+                    Console.WriteLine(string.Join("", wordTwo));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(string.Join("", wordTwo));
+                    Console.WriteLine(string.Join("", wordOne));
+                    break;
+                }
+            }
+        }
+    }
+}
